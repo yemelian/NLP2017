15c2505335de8797f5c8e3ad1f89589919703463
yemelian <yuriem@gmail.com>
2017-04-11 17:05:51 +0300

    Update hw2.py
    Optimized a little bit + added parameters support (argv)

--- hw2.py
+++ hw2.py
@@ -4,15 +4,15 @@ from os.path import isfile, join
 from collections import Counter
 import numpy as np
 
-bigrams={}
+bigrams={}  #all of bigrams will be there in that var
 frequencyOfEachWord={}
 pmiResults={}
-wordsTotal=0
+wordsTotal=0 # unigrams - counter for all words in corpuses
 
-def calculatePMI():
+def calculatePMI(): #PMI(x,y) = log(P(xy)/P(x)*P(y) * 1000
  global pmiResults
  global wordsTotal
-#PMI(x,y) = log(P(xy)/P(x)*P(y) * 1000
+
  for everyElement in bigrams:
    Pxy=bigrams[everyElement]
    Px=frequencyOfEachWord[everyElement[0]]
@@ -20,21 +20,16 @@ def calculatePMI():
    PMI=math.log((Pxy/Px*Py),2)*1000/wordsTotal
    pmiResults[everyElement]=PMI
 
-def calculate_Unigrams():
-    # Define paths to corpuses
-    mypathENG = r'C:\Temp\NLP\testset_literature'
-    mypathHEB = r'C:\Temp\NLP\haaretz.heb'
-    filesENG = [f for f in listdir(mypathENG) if isfile(os.path.join(mypathENG, f))]
-    filesHEB = [f for f in listdir(mypathHEB) if isfile(os.path.join(mypathHEB, f))]
-    # unigrams - counter for all words in corpuses
-    #wordsTotal = 0
-    #all of bigrams will be there in that var
+def calculate_Unigrams(argv):
     global bigrams
     global frequencyOfEachWord
     global wordsTotal
+    # Define paths to corpuses: sys.argv[1] = r'C:\Temp\NLP\testset_literature' sys.argv[2] = r'C:\Temp\NLP\haaretz.heb'
+    filesENG = [f for f in listdir(sys.argv[1]) if isfile(os.path.join(sys.argv[1], f))]
+    filesHEB = [f for f in listdir(sys.argv[2]) if isfile(os.path.join(sys.argv[2], f))]
 
     for name in filesENG:
-        with open(mypathENG + '\\' + name, encoding="utf8") as f:
+        with open(sys.argv[1] + '\\' + name, encoding="utf8") as f:
             words = [word for line in f for word in line.split()]
             # wordsTotal=wordsTotal+Counter(words)
             # Counting words for every file and su,,arize it with previous value - testset_literature
@@ -49,7 +44,7 @@ def calculate_Unigrams():
         #Calculating P(word) - frequency at corpus - END
 
         #calculating Bigrams P(word1, word2) in ENG start
-        with open (mypathENG + '\\' + name, encoding="utf8") as myfile:
+        with open (sys.argv[1] + '\\' + name, encoding="utf8") as myfile:
             data=myfile.readlines()
             newBigrams=[b for l in data for b in zip(l.split()[:-1], l.split()[1:])]
         #Bigram adding
@@ -61,7 +56,7 @@ def calculate_Unigrams():
 
      #calculating Bigrams in HEB start
     for name in filesHEB:
-        with open(mypathHEB + '\\' + name, encoding="utf8") as f:
+        with open(sys.argv[2] + '\\' + name, encoding="utf8") as f:
             words = [word for line in f for word in line.split()]
             # wordsTotal = wordsTotal + Counter(words)
             # Counting words for every file and su,,arize it with previous value - haaretz.heb
@@ -76,7 +71,7 @@ def calculate_Unigrams():
         #Calculating P(word) - frequency at corpus - END
 
         #calculating Bigrams P(word1, word2) in HEB start
-        with open (mypathHEB + '\\' + name, encoding="utf8") as myfile2:
+        with open (sys.argv[2] + '\\' + name, encoding="utf8") as myfile2:
             data2=myfile2.readlines()
             newBigrams = [b for l in data2 for b in zip(l.split()[:-1], l.split()[1:])]
         for everyElement in newBigrams:
@@ -89,8 +84,15 @@ def calculate_Unigrams():
     return wordsTotal
 pass
 
+
+# validate arguments length before continuing
+if len(sys.argv) != 4:
+    sys.exit('Invalid argument number!, please make sure you run the the command as follow: '
+                 'python hw2.py <FolderWithInputFiles1>  <FolderWithInputFiles2>  <FolderForOutputFiles>')
+# validate arguments length before continuing
+
 if __name__ == "__main__":
-    print(calculate_Unigrams())
+    print(calculate_Unigrams(sys.argv))
     calculatePMI()
     for key, value in pmiResults.items():
         print(key,value)
