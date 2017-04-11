bb7cf8f193511e1d33d8afdd5ec8a36325e9e331
yemelian <yuriem@gmail.com>
2017-04-11 17:28:16 +0300

    Update hw2.py
    50% done - bigram tasks completed

--- hw2.py
+++ hw2.py
@@ -2,17 +2,34 @@ import sys,os,math
 from os import listdir
 from os.path import isfile, join
 from collections import Counter
-import numpy as np
 
 bigrams={}  #all of bigrams will be there in that var
 frequencyOfEachWord={}
 pmiResults={}
+tTestResults={}
+X2TestResults={}
 wordsTotal=0 # unigrams - counter for all words in corpuses
 
-def calculatePMI(): #PMI(x,y) = log(P(xy)/P(x)*P(y) * 1000
- global pmiResults
- global wordsTotal
+def calculateX2Test(): #x = [ P(xy)-P(x)P(y) ] / [P(x)P(y)]
+ global X2TestResults
+ for everyElement in bigrams:
+   Pxy=bigrams[everyElement]
+   Px=frequencyOfEachWord[everyElement[0]]
+   Py=frequencyOfEachWord[everyElement[1]]
+   x2Test=(Pxy-Px*Py)/Px*Py
+   X2TestResults[everyElement]=x2Test
 
+def calculateTtest(): #t = [ P(xy)-P(x)P(y) ] / [sqrt(P(xy)/N)    N=wordsTotal
+ global tTestResults
+ for everyElement in bigrams:
+   Pxy=bigrams[everyElement]
+   Px=frequencyOfEachWord[everyElement[0]]
+   Py=frequencyOfEachWord[everyElement[1]]
+   tTest=(Pxy-Px*Py)/math.sqrt(Pxy)/wordsTotal
+   tTestResults[everyElement]=tTest
+
+def calculatePMI(): #PMI(x,y) = log(P(xy)/P(x)*P(y) * 1000/N      N=wordsTotal
+ global pmiResults
  for everyElement in bigrams:
    Pxy=bigrams[everyElement]
    Px=frequencyOfEachWord[everyElement[0]]
@@ -28,7 +45,7 @@ def calculate_Unigrams(argv):
     filesENG = [f for f in listdir(sys.argv[1]) if isfile(os.path.join(sys.argv[1], f))]
     filesHEB = [f for f in listdir(sys.argv[2]) if isfile(os.path.join(sys.argv[2], f))]
 
-    for name in filesENG:
+    for name in filesENG: #calculating Bigrams in  testset_literature start
         with open(sys.argv[1] + '\\' + name, encoding="utf8") as f:
             words = [word for line in f for word in line.split()]
             # wordsTotal=wordsTotal+Counter(words)
@@ -43,7 +60,7 @@ def calculate_Unigrams(argv):
             frequencyOfEachWord[everyElement]=1
         #Calculating P(word) - frequency at corpus - END
 
-        #calculating Bigrams P(word1, word2) in ENG start
+        #calculating Bigrams P(word1, word2) in 1st corpus: start
         with open (sys.argv[1] + '\\' + name, encoding="utf8") as myfile:
             data=myfile.readlines()
             newBigrams=[b for l in data for b in zip(l.split()[:-1], l.split()[1:])]
@@ -54,8 +71,7 @@ def calculate_Unigrams(argv):
          else:
             bigrams[everyElement]=1
 
-     #calculating Bigrams in HEB start
-    for name in filesHEB:
+    for name in filesHEB: #calculating Bigrams in  haaretz.heb start
         with open(sys.argv[2] + '\\' + name, encoding="utf8") as f:
             words = [word for line in f for word in line.split()]
             # wordsTotal = wordsTotal + Counter(words)
@@ -70,7 +86,7 @@ def calculate_Unigrams(argv):
             frequencyOfEachWord[everyElement]=1
         #Calculating P(word) - frequency at corpus - END
 
-        #calculating Bigrams P(word1, word2) in HEB start
+        #calculating Bigrams P(word1, word2) in 2nd corpus: start
         with open (sys.argv[2] + '\\' + name, encoding="utf8") as myfile2:
             data2=myfile2.readlines()
             newBigrams = [b for l in data2 for b in zip(l.split()[:-1], l.split()[1:])]
@@ -84,7 +100,6 @@ def calculate_Unigrams(argv):
     return wordsTotal
 pass
 
-
 # validate arguments length before continuing
 if len(sys.argv) != 4:
     sys.exit('Invalid argument number!, please make sure you run the the command as follow: '
@@ -94,5 +109,11 @@ if len(sys.argv) != 4:
 if __name__ == "__main__":
     print(calculate_Unigrams(sys.argv))
     calculatePMI()
-    for key, value in pmiResults.items():
+    #for key, value in pmiResults.items():
+    #    print(key,value)
+    calculateTtest()
+    #for key, value in tTestResults.items():
+    #    print(key,value)
+    calculateX2Test()
+    for key, value in X2TestResults.items():
         print(key,value)
