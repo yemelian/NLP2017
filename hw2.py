a8a368bc3d60290817313cf140bb77235c1bdfda
Yuri Emelianov <yuriem@gmail.com>
2017-04-17 15:38:57 +0300

    Update hw2.py
    Bugs at formulas - fixed

--- hw2.py
+++ hw2.py
@@ -15,10 +15,24 @@ X3TrigramsBResults={}
 wordsTotal=0 # unigrams - counter for all words in corpuses
 bigramsTotal=0 # bigrams - counter for all bigrams in corpuses
 trigramsTotal=0 # trigrams - counter for all trigrams in corpuses
+mutualTrigramsFromResults={}
+
+def calculateMutualCollacationsTrigram():
+    global TtestTrigramsAResults
+    global TtestTrigramsBResults
+    global X3TrigramsAResults
+    global X3TrigramsBResults
+    intersect = []
+    TtestTrigramsAResults=sorted(TtestTrigramsAResults.items(), key=lambda x: (-x[1], x[0]),reverse=True)[:10000]
+    TtestTrigramsBResults=sorted(TtestTrigramsBResults.items(), key=lambda x: (-x[1], x[0]),reverse=True)[:10000]
+    X3TrigramsAResults=sorted(X3TrigramsAResults.items(), key=lambda x: (-x[1], x[0]),reverse=True)[:10000]
+    X3TrigramsBResults=sorted(X3TrigramsBResults.items(), key=lambda x: (-x[1], x[0]),reverse=True)[:10000]
+    intersect=set(TtestTrigramsAResults) & set(TtestTrigramsBResults) & set(X3TrigramsAResults) & set(X3TrigramsBResults)
+    print(intersect)
 
 def saveTop100Results(fileName2Save, resultsToSave): # saving top 100 results from any dictionary that passed
  file = io.open(fileName2Save, 'w+', encoding='utf8')
- for key,value in sorted(resultsToSave.items(), key=lambda x: (-x[1], x[0]))[:100]:
+ for key,value in sorted(resultsToSave.items(), key=lambda x: (-x[1], x[0]),reverse=True)[:100]:
      file.write(str(key)+" "+str(value)+"\n")
  file.close()
 
@@ -68,31 +82,31 @@ def calculateX3TestTrigramsB(): # x3_b =  [ P(xyz)-P(xy)P(yz) ] / [P(xy)P(yz)]
    X3TrigramsBResults[everyTrigramElement]=float("{0:.3f}".format((Pxyz-Pxy*Pyz)/ (Pxy*Pyz)))
  saveTop100Results('xtest_tri_b.txt',X3TrigramsBResults)  # saving top 100 results: xtest_tri_b.txt
 
-def calculateX2TestBigrams(): #x = [ P(xy)-P(x)P(y) ] / [P(x)P(y)] N=wordsTotal
+def calculateX2TestBigrams(): #x = [ P(xy)-P(x)P(y) ] / [P(x)P(y)]
  global X2TestResults
  for everyElement in bigrams:
   if frequencyOfEachWord[everyElement[0]]>=20 and frequencyOfEachWord[everyElement[1]]>=20: #Px and Py should be greater than 20
-   Pxy=bigrams[everyElement]/bigramsTotal
+   Pxy=bigrams[tuple(everyElement)]/bigramsTotal
    Px=frequencyOfEachWord[everyElement[0]]/wordsTotal
    Py=frequencyOfEachWord[everyElement[1]]/wordsTotal
-   X2TestResults[everyElement]=float("{0:.3f}".format((Pxy-Px*Py)/Px*Py))
+   X2TestResults[everyElement]=float("{0:.3f}".format((Pxy-Px*Py)/(Px*Py)))
  saveTop100Results('xtest_pair.txt',X2TestResults)  # saving top 100 results:   xtest_pair.txt
 
 def calculateTtestBigrams(): #t = [ P(xy)-P(x)P(y) ] / [sqrt(P(xy)/N)    N=wordsTotal
  global tTestResults
  for everyElement in bigrams:
    if frequencyOfEachWord[everyElement[0]]>=20 and frequencyOfEachWord[everyElement[1]]>=20: #Px and Py should be greater than 20
-    Pxy=bigrams[everyElement]/bigramsTotal
+    Pxy=bigrams[tuple(everyElement)]/bigramsTotal
     Px=frequencyOfEachWord[everyElement[0]]/wordsTotal
     Py=frequencyOfEachWord[everyElement[1]]/wordsTotal
-    tTestResults[everyElement]=float("{0:.3f}".format((Pxy-Px*Py)/math.sqrt(Pxy)/wordsTotal))
+    tTestResults[everyElement]=float("{0:.3f}".format((Pxy-Px*Py)/ math.sqrt(Pxy/wordsTotal)))
  saveTop100Results('ttest_pair.txt',tTestResults)  # saving top 100 results: ttest_pair.txt
 
 def calculatePMIBigrams(): #PMI(x,y) = log(P(xy)/P(x)*P(y)
  global pmiResults
  for everyElement in bigrams:
   if frequencyOfEachWord[everyElement[0]]>=20 and frequencyOfEachWord[everyElement[1]]>=20: #Px and Py should be greater than 20
-   Pxy=bigrams[everyElement]/bigramsTotal
+   Pxy=bigrams[tuple(everyElement)]/bigramsTotal
    Px=frequencyOfEachWord[everyElement[0]]/wordsTotal
    Py=frequencyOfEachWord[everyElement[1]]/wordsTotal
    pmiResults[everyElement]=float("{0:.3f}".format(math.log((Pxy/Px*Py),2)))
@@ -139,12 +153,12 @@ def calculate_Unigrams(argv):
     global wordsTotal
     global bigramsTotal
     # Define paths to corpuses: sys.argv[1] = r'C:\Temp\NLP\testset_literature' sys.argv[2] = r'C:\Temp\NLP\haaretz.heb'
-    filesENG = [f for f in listdir(sys.argv[1]) if isfile(os.path.join(sys.argv[1], f))]
-    filesHEB = [f for f in listdir(sys.argv[2]) if isfile(os.path.join(sys.argv[2], f))]
+    filesFirstCorpus = [f for f in listdir(sys.argv[1]) if isfile(os.path.join(sys.argv[1], f))]
+    filesSecondCorpus = [f for f in listdir(sys.argv[2]) if isfile(os.path.join(sys.argv[2], f))]
 
-    for name in filesENG: #calculating Bigrams in  testset_literature start
-        with open(sys.argv[1] + '\\' + name, encoding="utf8") as f:
-            words = [word for line in f for word in line.split()]
+    for name in filesFirstCorpus: #calculating Bigrams in  testset_literature start
+        with open(sys.argv[1] + '\\' + name, encoding="utf8") as fileForWordsCount:
+            words = [word for line in fileForWordsCount for word in line.split()]
             # wordsTotal=wordsTotal+Counter(words)
             # Counting words for every file and su,,arize it with previous value - testset_literature
             wordsTotal = wordsTotal + len(words)
@@ -156,20 +170,22 @@ def calculate_Unigrams(argv):
          else:
             frequencyOfEachWord[everyElement]=1
         #Calculating P(word) - frequency at corpus - END
+        fileForWordsCount.close()
 
         #calculating Bigrams P(word1, word2) in 1st corpus: start
-        with open (sys.argv[1] + '\\' + name, encoding="utf8") as myfile:
-            data=myfile.readlines()
+        with open (sys.argv[1] + '\\' + name, encoding="utf8") as bigramFile:
+            data=bigramFile.readlines()
             newBigrams=[b for l in data for b in zip(l.split()[:-1], l.split()[1:])]
         #Bigram adding
         for everyElement in newBigrams:
-         if everyElement in bigrams.keys():
-            bigrams[everyElement]+=1
+         if tuple(everyElement) in bigrams.keys():
+            bigrams[tuple(everyElement)]+=1
          else:
-            bigrams[everyElement]=1
+            bigrams[tuple(everyElement)]=1
             bigramsTotal+=1
+         bigramFile.close()
 
-    for name in filesHEB: #calculating Bigrams in  haaretz.heb start
+    for name in filesSecondCorpus: #calculating Bigrams in  haaretz.heb start
         with open(sys.argv[2] + '\\' + name, encoding="utf8") as f:
             words = [word for line in f for word in line.split()]
             # wordsTotal = wordsTotal + Counter(words)
@@ -189,13 +205,12 @@ def calculate_Unigrams(argv):
             data2=myfile2.readlines()
             newBigrams = [b for l in data2 for b in zip(l.split()[:-1], l.split()[1:])]
         for everyElement in newBigrams:
-         if everyElement in bigrams.keys():
-            bigrams[everyElement]+=1
+         if tuple(everyElement) in bigrams.keys():
+            bigrams[tuple(everyElement)]+=1
          else:
-            bigrams[everyElement]=1
+            bigrams[tuple(everyElement)]=1
             bigramsTotal+=1
     file = io.open('freq_raw.txt', 'w+', encoding='utf8')       # saving: freq_raw.txt
-    #for key,value in bigrams.items():
     for key,value in sorted(bigrams.items(), key=lambda x: (-x[1], x[0])):
       file.write(str(key)+" "+str(float("{0:.3f}".format((value/wordsTotal)*1000)))+"\n") #multiply by 1000
     file.close()
@@ -211,8 +226,9 @@ if __name__ == "__main__":
     calculatePMIBigrams()
     calculateTtestBigrams()
     calculateX2TestBigrams()
-    calculateTrigrams(sys.argv)
-    calculateTtestTrigramsA()
-    calculateTtestTrigramsB()
-    calculateX3TestTrigramsA()
-    calculateX3TestTrigramsB()
+    #calculateTrigrams(sys.argv)
+    #calculateTtestTrigramsA()
+    #calculateTtestTrigramsB()
+    #calculateX3TestTrigramsA()
+    #calculateX3TestTrigramsB()
+    #calculateMutualCollacationsTrigram()
