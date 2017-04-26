26eabc19c625530fbee64157e5ee8c4eb00b5f93
Yuri Emelianov <yuriem@gmail.com>
2017-04-26 23:50:56 +0300

    Update hw3.py
    start! :)

--- hw3.py
+++ hw3.py
@@ -1,1 +1,11 @@
 
+def featureClassification(argv):
+
+if len(sys.argv) != 4: # validate arguments length before continuing ##############################################
+    sys.exit('Invalid argument number!, please make sure you run the the command as follow: '
+                 'python hw3.py <input_file> <words_file_input_path> <best_words_file_output_path>
+# validate arguments length before continuing #####################################################################
+
+if __name__ == "__main__":
+    ##### Questions 1 results
+   featureClassification(sys.argv)
