26e4ce3982907c333f89cc2068b5d8c403dfcc69
Yuri Emelianov <yuriem@gmail.com>
2017-04-28 23:38:14 +0300

    Update hw3.py

--- hw3.py
+++ hw3.py
@@ -1,18 +1,18 @@
 import sys,os,math,io, itertools
 
 
- def featureClassification(argv):
+def featureClassification(argv):
     # Get 50 english words
     file = io.open(sys.argv[2], 'w+', encoding='utf8')
 
     file.close()
- pass
+    pass
 
- if len(sys.argv) != 4: # validate arguments length before continuing ##############################################
+if len(sys.argv) != 4: # validate arguments length before continuing ##############################################
     sys.exit('Invalid argument number!, please make sure you run the the command as follow: '
-                 'python hw3.py <input_file> <words_file_input_path> <best_words_file_output_path>
+                 'python hw3.py <input_file> <words_file_input_path> <best_words_file_output_path>')
    # validate arguments length before continuing #####################################################################
 
- if __name__ == "__main__":
+if __name__ == "__main__":
     ##### Questions 1 results
   featureClassification(sys.argv)
