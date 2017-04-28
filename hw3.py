38d82ac7f85b9133e410c73b659d2b89e689cb70
Yuri Emelianov <yuriem@gmail.com>
2017-04-29 00:11:41 +0300

    Update hw3.py
    Paragraphs 1a and 1b (starting getting songs)

--- hw3.py
+++ hw3.py
@@ -3,9 +3,19 @@ import sys,os,math,io, itertools
 
 def featureClassification(argv):
     # Get 50 english words
-    file = io.open(sys.argv[2], 'w+', encoding='utf8')
+    wordsTop50 = []
+    #fileTop50 = io.open(sys.argv[2], 'w+', encoding='utf8')
+    with open(sys.argv[2], "r") as fileTop50:
+        for line in fileTop50.readlines():
+            wordsTop50.append(line.replace("\n", ""))
+    fileTop50.close()
+    # Get 400 songs
+    #songsToProcess = io.open(sys.argv[1], 'w+', encoding='utf8')
+    with open(sys.argv[1], "r", encoding='utf8') as songsToProcess:
+        for line in songsToProcess.readline():
+          print(line)
+    songsToProcess.close()
 
-    file.close()
     pass
 
 if len(sys.argv) != 4: # validate arguments length before continuing ##############################################
