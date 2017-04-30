f7b636bd1f789f44d2b5d0c5d9ecb5458dd4f236
Jawad Margieh <J.margieh@gmail.com>
2017-04-30 13:30:48 +0300

    strip percuations marks from words

--- hw3.py
+++ hw3.py
@@ -5,6 +5,8 @@ from sklearn import tree
 from sklearn.svm import SVC
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.neighbors import KNeighborsClassifier
+import string
+import re
 
 result = {}
 featureVectorsBeatles=[]
@@ -57,6 +59,17 @@ def get_lyrics_from_csv_by_artist(lyrics_file_name, artists, maximum_songs_numbe
 pass
 
 
+def split_and_remove_punctuations(lyrics):
+
+    lyrics = lyrics.split()
+
+    for i in range(len(lyrics)):
+        lyrics[i] = lyrics[i].strip(string.punctuation)
+
+    return lyrics
+pass
+
+
 def featureVectorBuild(argv):
     global featureVectorsBeatles
     global featureVectorsBreatney
@@ -64,17 +77,17 @@ def featureVectorBuild(argv):
     wordsTop50ToCheck={}
     with open(argv[2], "r") as fileTop50:
         for line in fileTop50.readlines():
-            wordsTop50.append(line.replace("\n", ""))
+            wordsTop50.append(line.replace("\n", "").replace("'", "").replace(",", ""))
         fileTop50.close()
     for element in wordsTop50:
         wordsTop50ToCheck[element] = 0
 
-    #Beatles
+    # Beatles
     resultsForBeatles=result["beatles"]
     for lyricToCheck in resultsForBeatles:
         wordsTop50ToCheckTemp = wordsTop50ToCheck
-        lyricToCheckList = lyricToCheck.split()
-        for key in wordsTop50ToCheckTemp.items():
+        lyricToCheckList = split_and_remove_punctuations(lyricToCheck)
+        for key, value in wordsTop50ToCheckTemp.items():
             for lyricToken in lyricToCheckList:
                 if (key.strip().lower().replace('\'', '')) in (lyricToken.strip().lower()):
                     wordsTop50ToCheckTemp[key] = 1
@@ -83,12 +96,13 @@ def featureVectorBuild(argv):
         for element in wordsTop50ToCheckTemp:
             wordsTop50ToCheckTemp[element] = 0
 
-    #Britney
+
+    # Britney
     resultsForBreatney=result["britney-spears"]
     for lyricToCheck in resultsForBreatney:
         wordsTop50ToCheckTemp = wordsTop50ToCheck
-        lyricToCheckList = lyricToCheck.split()
-        for key in wordsTop50ToCheckTemp.items():
+        lyricToCheckList = split_and_remove_punctuations(lyricToCheck)
+        for key, value in wordsTop50ToCheckTemp.items():
             for lyricToken in lyricToCheckList:
                 if (key.strip().lower().replace('\'', '')) in (lyricToken.strip().lower()):
                     wordsTop50ToCheckTemp[key] = 1
