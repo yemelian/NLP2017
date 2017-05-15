68913fe53de0b9771b158e8ddea8e3b5b561be98
Yuri Emelianov <yuriem@gmail.com>
2017-05-15 23:12:58 +0300

    Update hw4.py

--- hw4.py
+++ hw4.py
@@ -1,6 +1,10 @@
+import nltk
+
+
 if __name__ == "__main__":
 
     if len(sys.argv) != 4:
         sys.exit('Invalid argument number!, please make sure you run the the command as follow: python hw4.py'
                  '<line.data.train> <line.data.test> <output>')
-
+    sentence = """At eight o'clock on Thursday morning
+    tokens = nltk.word_tokenize(sentence)
