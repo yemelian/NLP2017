18a2974c0550ef4f19897cd4f08307b7c076c776
Yuri Emelianov <yuriem@gmail.com>
2017-05-15 22:48:49 +0300

    Update hw4.py

--- hw4.py
+++ hw4.py
@@ -1,1 +1,6 @@
+if __name__ == "__main__":
+
+    if len(sys.argv) != 4:
+        sys.exit('Invalid argument number!, please make sure you run the the command as follow: python hw4.py'
+                 '<line.data.train> <line.data.test> <output>')
 
