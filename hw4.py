ae7c969b1eae293cecf1e88e7e93d303d16c2aa9
Yuri Emelianov <yuriem@gmail.com>
2017-05-18 23:36:06 +0300

    Update hw4.py
    TOKENIZATION  for features seems to be ready (did not check at 100%)

--- hw4.py
+++ hw4.py
@@ -1,19 +1,19 @@
 import sys
 import nltk
 from copy import deepcopy
-from sklearn.linear_model import linearregression
+from sklearn.linear_model import LogisticRegression
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import cross_val_score
 import xml.etree.ElementTree as ET
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer, ENGLISH_STOP_WORDS
 from sklearn.feature_selection import SelectKBest
 from gensim import models
-from auxiliary_class import Instance
 
+tokensForFeatures={}
 all_instances= []
 
 def ClassifierLinearRegression(data_points, true_values):
-    trained_svc = linearregression().fit(data_points, true_values)
+    trained_svc = LogisticRegression().fit(data_points, true_values)
     scores = cross_val_score(trained_svc, data_points, true_values, cv=10)
     print("accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
 
@@ -21,13 +21,35 @@ pass
 
 def bag_of_words(argv, voc=None):
     global all_instances
+    global tokensForFeatures
+    contextData = ""
+
+    #content from line.data.TEST.xml
     etree = ET.parse(str(sys.argv[1]))
     root = etree.getroot()
     instances = root.findall(".//instance")
     for element in instances:
-        instance = Instance()
-        instance.parsing_instance_to_object(element)
-    all_instances.append(instance)
+        for child in element.iter():
+            #sub_tags = list(child.attrib)
+            #for tag in sub_tags:
+            if child.tag == "context":
+                for sub_child in child.iter():
+                    contextData += sub_child.text + "\r\n"
+
+    #content from line.data.TRAIN.xml
+    etree = ET.parse(str(sys.argv[2]))
+    root = etree.getroot()
+    instances = root.findall(".//instance")
+    for element in instances:
+        for child in element.iter():
+            #sub_tags = list(child.attrib)
+            #for tag in sub_tags:
+            if child.tag == "context":
+                for sub_child in child.iter():
+                    contextData += sub_child.text + "\r\n"
+
+    #tokenization process
+    tokensForFeatures=nltk.word_tokenize(contextData.strip())
 
     # build true values vector
     true_values = ["beatles" for x in range(len(lyrics_by_artist_dic['beatles']))]
@@ -35,10 +57,10 @@ def bag_of_words(argv, voc=None):
 
     # merge lyrics together
     data = []
-    data.extend(lyrics_by_artist_dic["beatles"])
-    data.extend(lyrics_by_artist_dic["britney-spears"])
+    #data.extend(lyrics_by_artist_dic["beatles"])
+    #data.extend(lyrics_by_artist_dic["britney-spears"])
 
-    data = [var for var in data if var]
+    #data = [var for var in data if var]
 
     # apply CountVectonizer and tf-idf
     count_vectorizer = CountVectorizer(stop_words=ENGLISH_STOP_WORDS, vocabulary=voc)
@@ -66,6 +88,5 @@ if __name__ == "__main__":
     bag_of_words(sys.argv)
 
     # Question number 2
-    loadEmbeddingsFile(sys.argv)
-    sentence = "At eight o'clock on Thursday morning"
-    tokens = nltk.word_tokenize(sentence)
+    #loadEmbeddingsFile(sys.argv)
+
