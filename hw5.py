fc8cdb4d7d943e26f26e3606e65af19cd9d9e75b
Yuri Emelianov <yuriem@gmail.com>
2017-06-02 19:49:51 +0300

    Update hw5.py
    getting rammar

--- hw5.py
+++ hw5.py
@@ -1,12 +1,36 @@
 import os
 import sys
+import nltk
 import string
 import numpy
+from sklearn.metrics import accuracy_score, f1_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import cross_val_score
+from sklearn import svm, neighbors, tree, naive_bayes
 from xml.etree import ElementTree
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer, ENGLISH_STOP_WORDS
+from sklearn.feature_selection import SelectKBest
+from sklearn.feature_extraction.text import TfidfVectorizer, TfidfTransformer
+from gensim import models
+from sklearn.preprocessing import MaxAbsScaler
 
+grammarList={}
+
+## Getting rammar from file
+## Every member at dicionary:
+## Value is probabillity mumber
+## Key is a rule of form LeftSide"*"RightSide
+## Example for  input 0.2 V -> stops will be: Value=0.2 Key=V*stops
 def getGrammar(argv):
-    print("grammar")
-
+ global grammarList
+ with open(argv[1], "r") as grammarFileData:
+     for line in grammarFileData.readlines():
+         newList=line.split("->")
+         newValue=newList[0].split(" ")
+         newKey=newValue[1].strip()+"*"+newList[1].strip()
+         grammarList[newKey] = newValue[0]
+     grammarFileData.close()
 pass
 
 
